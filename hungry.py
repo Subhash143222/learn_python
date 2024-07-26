dd0c74d07125104c44a3cabf0ce0ce8195dbf388
Subhash Bishnoi <subhashbishnoi@Subhashs-MacBook-Air.local>
2024-07-26 02:17:09 -0700

    thristy

--- hungry.py
+++ hungry.py
@@ -2,6 +2,11 @@ hungry=input("are you hungry(answer in yes or no):")
 if hungry =="yes":
     print("eat  samosa")
     print("eat pizza")
-    print("eat fries")
+elif hungry=="no":
+    thristy=input("are you thristy")
+    if thristy=="yes":
+        print("drink water")
+        print("drink soda")
+
 else:
     print("do homework")
