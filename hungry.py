a43593482696e876a570de62508f2e740ee382c9
Subhash Bishnoi <subhashbishnoi@Subhashs-MacBook-Air.local>
2024-07-26 01:47:16 -0700

    hungry

--- hungry.py
+++ hungry.py
@@ -3,12 +3,6 @@ if hungry =="yes":
     print("eat  samosa")
     print("eat pizza")
 elif hungry=="no":
-    thirsty=input("are you thirsty:")
-    if thirsty=="yes":
-        print("drink water")
-    elif thirsty=="no":
-        print("do your homework")
-    else:
-        print("im not able to understand your answer")
+    print("do homework")
 else:
     print("im not able to understand your answer")
