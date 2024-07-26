95aeee607610e502242dcb5cedffc63a46c29e3d
Subhash Bishnoi <subhashbishnoi@Subhashs-MacBook-Air.local>
2024-07-25 23:56:56 -0700

    2nd commit for hungry.py

--- hungry.py
+++ hungry.py
@@ -1,1 +1,7 @@
-print("I'm hungry")
+hungry=input("are you hungry(answer in yes or no):")
+if hungry =="yes":
+    print("eat  samosa")
+elif hungry=="no":
+    print("do your homework")
+else:
+    print("im not able to understand your answer")
